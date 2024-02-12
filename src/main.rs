fe1bd7ec0d4b8b87718b7e15799cf4020f056077
忍和 <90991904+non7247@users.noreply.github.com>
2024-02-12 07:42:07 +0000

    winodws()の使用例

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3];
+    for w in v.windows(3) {
+        let (prev, current, next) = (w[0], w[1], w[2]);
+        println!("{} {} {}", prev, current, next);
+    }
+
+    for (prev, next) in v.iter().tuple_windows() {
+        println!("{} {}", prev, next);
+    }
+
+    let v = vec![1, 2, 3, 4, 5];
+    let b1 = v.windows(2).all(|a| a[0] < a[1]);
+    let b2 = v.iter().tuple_windows().all(|(p, n)| p < n);
+    // returns true
+    println!("{}", b1);
+    println!("{}", b2);
+
+    let v = vec![1, 2, 4, 3, 5];
+    let b1 = v.windows(2).all(|a| a[0] < a[1]);
+    let b2= v.iter().tuple_windows().all(|(p, n)| p < n);
+    // returns false
+    println!("{}", b1);
+    println!("{}", b2);
 }
