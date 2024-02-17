21a19d0749af65712e8e2bdd05d781a937978290
忍和 <90991904+non7247@users.noreply.github.com>
2024-02-17 02:28:09 +0000

    chain()でイテレータをつなぐ例

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,13 @@
-use itertools::Itertools;
-
 fn main() {
-    let v = vec![1, 2, 3];
-    for w in v.windows(3) {
-        let (prev, current, next) = (w[0], w[1], w[2]);
-        println!("{} {} {}", prev, current, next);
-    }
+    let stream = vec!["a".to_string(), "b".to_string(), "c".to_string()]
+        .into_iter()
+        .cycle()
+        .take(10)
+        .chain(
+            (0..10).map(|i| format!("{}", i))
+        )
+        .chain(vec!["a".to_string(), "b".to_string(), "c".to_string()])
+        .collect::<Vec<_>>();
 
-    for (prev, next) in v.iter().tuple_windows() {
-        println!("{} {}", prev, next);
-    }
-
-    let v = vec![1, 2, 3, 4, 5];
-    let b1 = v.windows(2).all(|a| a[0] < a[1]);
-    let b2 = v.iter().tuple_windows().all(|(p, n)| p < n);
-    // returns true
-    println!("{}", b1);
-    println!("{}", b2);
-
-    let v = vec![1, 2, 4, 3, 5];
-    let b1 = v.windows(2).all(|a| a[0] < a[1]);
-    let b2= v.iter().tuple_windows().all(|(p, n)| p < n);
-    // returns false
-    println!("{}", b1);
-    println!("{}", b2);
+    println!("{:?}", stream);
 }
